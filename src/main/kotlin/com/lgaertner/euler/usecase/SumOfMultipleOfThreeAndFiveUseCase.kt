d8f9304aecd92b42d680c2cb4eb2b642b8765039
Ludwig Gaertner <lgaertner@simscale.com>
2023-04-17 23:06:13 +0200

    working

--- src/main/kotlin/com/lgaertner/euler/usecase/SumOfMultipleOfThreeAndFiveUseCase.kt
+++ src/main/kotlin/com/lgaertner/euler/usecase/SumOfMultipleOfThreeAndFiveUseCase.kt
@@ -1,9 +1,12 @@
 package com.lgaertner.euler.usecase
 
+import com.lgaertner.euler.math.Sum
+
 class SumOfMultipleOfThreeAndFiveUseCase {
     fun execute(maxValue: Int) : Int {
         val values = List(maxValue) { it + 1 }
         val filteredValues = values.filter{it % 3 == 0 || it % 5 == 0}
-        return filteredValues.sum()
+        val sum = Sum(filteredValues)
+        return sum.value()
     }
 }
