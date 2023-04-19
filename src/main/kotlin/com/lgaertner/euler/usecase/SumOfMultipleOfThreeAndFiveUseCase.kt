628e3b8c8fc309aa15176234f32042d5eec7f4dc
lgaertner <lgaertner@simscale.com>
2023-04-19 22:59:12 +0200

    working

--- src/main/kotlin/com/lgaertner/euler/usecase/SumOfMultipleOfThreeAndFiveUseCase.kt
+++ src/main/kotlin/com/lgaertner/euler/usecase/SumOfMultipleOfThreeAndFiveUseCase.kt
@@ -2,13 +2,14 @@ package com.lgaertner.euler.usecase
 
 import com.lgaertner.euler.math.Range
 import com.lgaertner.euler.math.Sum
+import kotlin.math.max
 
 class SumOfMultipleOfThreeAndFiveUseCase {
     fun execute(maxValue: Int) : Int {
         if (maxValue < 1) throw IllegalArgumentException("maxValue must be greater than 0")
 
-        val range = Range(1, maxValue)
-        val filteredValues = range.values().map{if (it % 3 == 0 || it % 5 == 0) it else 0}.toList()
+        val range = List(maxValue) {it + 1}
+        val filteredValues = range.map{if (it % 3 == 0 || it % 5 == 0) it else 0}.toList()
         val sum = Sum(filteredValues)
         return sum.value()
     }
