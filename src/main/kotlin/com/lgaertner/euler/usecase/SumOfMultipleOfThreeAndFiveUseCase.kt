8c0c67c8dfa82455f35be0700655abd61811eb44
lgaertner <lgaertner@simscale.com>
2023-04-19 23:25:01 +0200

    working

--- src/main/kotlin/com/lgaertner/euler/usecase/SumOfMultipleOfThreeAndFiveUseCase.kt
+++ src/main/kotlin/com/lgaertner/euler/usecase/SumOfMultipleOfThreeAndFiveUseCase.kt
@@ -7,14 +7,16 @@ class SumOfMultipleOfThreeAndFiveUseCase {
         if (maxValue < 1) throw IllegalArgumentException("maxValue must be greater than 0")
 
         val values = List(maxValue) {it + 1}
-        val filteredValues = values.filter{Filter({it % 3 == 0 || it % 5 == 0}, it).apply()}.toList()
+        val filteredValues = values.map{Filter({it % 3 == 0 || it % 5 == 0}, it).apply()}.toList()
         val sum = Sum(filteredValues)
         return sum.value()
     }
 
     class Filter(private val filter : (Int)->Boolean, private val value: Int) {
-        fun apply() : Boolean {
-            return (filter(value))
+        fun apply() : Int {
+            return if (filter(value))
+                value
+            else 0
         }
     }
 }
