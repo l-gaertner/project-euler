c9f8656f7949d06c09cd7e0bc47ab7a87e47f54b
lgaertner <lgaertner@simscale.com>
2023-04-19 23:22:09 +0200

    working

--- src/main/kotlin/com/lgaertner/euler/math/Range.kt
+++
@@ -1,7 +0,0 @@
-package com.lgaertner.euler.math
-
-class Range(private val start: Int, private val end: Int) {
-    fun values(): Collection<Int> {
-        return List(end - start + 1) { it + start }
-    }
-}
--- src/main/kotlin/com/lgaertner/euler/usecase/SumOfMultipleOfThreeAndFiveUseCase.kt
+++ src/main/kotlin/com/lgaertner/euler/usecase/SumOfMultipleOfThreeAndFiveUseCase.kt
@@ -1,16 +1,24 @@
 package com.lgaertner.euler.usecase
 
-import com.lgaertner.euler.math.Range
 import com.lgaertner.euler.math.Sum
-import kotlin.math.max
 
 class SumOfMultipleOfThreeAndFiveUseCase {
     fun execute(maxValue: Int) : Int {
         if (maxValue < 1) throw IllegalArgumentException("maxValue must be greater than 0")
 
-        val range = List(maxValue) {it + 1}
-        val filteredValues = range.map{if (it % 3 == 0 || it % 5 == 0) it else 0}.toList()
+        val values = List(maxValue) {it + 1}
+        val filteredValues = values.filter{Filter({value -> value % 3 == 0 || value % 5 == 0}, it).apply()}.toList()
         val sum = Sum(filteredValues)
         return sum.value()
+    }
+
+    interface FilterChain {
+        fun apply(): Boolean
+    }
+
+    class Filter(private val filter : (Int)->Boolean, private val value: Int) : FilterChain {
+        override fun apply() : Boolean {
+            return (filter(value))
+        }
     }
 }
