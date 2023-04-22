12799dd5dae49d1a7d1b56e3d1fd9aac7abb534e
lgaertner <lgaertner@simscale.com>
2023-04-22 17:52:24 +0200

    working

--- src/test/kotlin/com/lgaertner/euler/usecase/SumOfEvenFibonaciUseCaseTest.kt
+++ src/test/kotlin/com/lgaertner/euler/usecase/SumOfEvenFibonaciUseCaseTest.kt
@@ -1,5 +1,6 @@
 package com.lgaertner.euler.usecase
 
+import junit.framework.TestCase.assertEquals
 import kotlin.test.Test
 
 class SumOfEvenFibonaciUseCaseTest {
@@ -9,6 +10,6 @@ class SumOfEvenFibonaciUseCaseTest {
     @Test
     fun `should return 0 for 1`() {
         val result = sumOfEvenFibonaciUseCase.execute(1)
-
+        assertEquals(0, result);
     }
 }
