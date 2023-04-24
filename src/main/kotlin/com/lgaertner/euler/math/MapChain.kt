658e76a10dfee895507966abcbeb865ac0605e55
lgaertner <lgaertner@simscale.com>
2023-04-24 17:52:46 +0200

    working

--- src/main/kotlin/com/lgaertner/euler/math/MapChain.kt
+++ src/main/kotlin/com/lgaertner/euler/math/MapChain.kt
@@ -4,17 +4,17 @@ interface MapChain<K> : Wrapper<K> {
     override fun value(): K
 }
 
-class Map<K>(private val filter: (K) -> Boolean, private val value: K, private val next: MapChain<K>) :
-    MapChain<K> {
-    override fun value(): K {
-        return if (filter(value))
-            value
-        else next.value()
-    }
-}
-
-class FinalMap<K>(private val defaultValue: K) : MapChain<K> {
-    override fun value(): K {
-        return defaultValue
-    }
-}
+//class Map<K>(private val filter: (K) -> Boolean, private val value: K, private val next: MapChain<K>) :
+//    MapChain<K> {
+//    override fun value(): K {
+//        return if (filter(value))
+//            value
+//        else next.value()
+//    }
+//}
+//
+//class FinalMap<K>(private val defaultValue: K) : MapChain<K> {
+//    override fun value(): K {
+//        return defaultValue
+//    }
+//}
