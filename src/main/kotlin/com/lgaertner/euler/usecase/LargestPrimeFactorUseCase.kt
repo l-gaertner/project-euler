bb5cf07bfce1a2de6ef2f91792fea5f9af8285b9
lgaertner <lgaertner@simscale.com>
2023-04-26 19:19:56 +0200

    working

--- src/main/kotlin/com/lgaertner/euler/usecase/LargestPrimeFactorUseCase.kt
+++ src/main/kotlin/com/lgaertner/euler/usecase/LargestPrimeFactorUseCase.kt
@@ -1,4 +1,7 @@
 package com.lgaertner.euler.usecase
 
 class LargestPrimeFactorUseCase {
+    fun execute(number: Int): Int {
+        return 0
+    }
 }
--- src/test/kotlin/com/lgaertner/euler/usecase/LargestPrimeFactorUseCaseTest.kt
+++ src/test/kotlin/com/lgaertner/euler/usecase/LargestPrimeFactorUseCaseTest.kt
@@ -4,4 +4,8 @@ import kotlin.test.Test
 
 class LargestPrimeFactorUseCaseTest {
     private val useCase = LargestPrimeFactorUseCase()
+    @Test
+    fun `should return 2 as largest prime factor of 2 `() {
+        val largestPrimeFactor = useCase.execute(2)
+    }
 }
