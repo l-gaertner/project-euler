40449b7fd072907986f5d098bad35f30e5b5c83b
Ludwig Gaertner <lgaertner@simscale.com>
2023-04-30 16:29:53 +0200

    working

--- src/test/kotlin/com/lgaertner/euler/usecase/LargestPrimeFactorUseCaseTest.kt
+++ src/test/kotlin/com/lgaertner/euler/usecase/LargestPrimeFactorUseCaseTest.kt
@@ -41,9 +41,9 @@ class LargestPrimeFactorUseCaseTest {
         assertEquals(2, largestPrimeFactor)
     }
     
-//    @Test
-//    fun `should return 6857 as largest prime factor of 600851475143` () {
-//        val largestPrimeFactor = useCase.execute(600851475143)
-//        assertEquals(6857, largestPrimeFactor)
-//    }
+    @Test
+    fun `should return 6857 as largest prime factor of 600851475143` () {
+        val largestPrimeFactor = useCase.execute(600851475143)
+        assertEquals(6857, largestPrimeFactor)
+    }
 }
