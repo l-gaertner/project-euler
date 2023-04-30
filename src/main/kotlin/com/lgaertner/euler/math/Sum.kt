6d194deb3fcd40c36491d0802f685d9a29c5e153
Ludwig Gaertner <lgaertner@simscale.com>
2023-04-30 16:23:14 +0200

    working

--- src/main/kotlin/com/lgaertner/euler/math/Sum.kt
+++ src/main/kotlin/com/lgaertner/euler/math/Sum.kt
@@ -1,6 +1,5 @@
 package com.lgaertner.euler.math
 
 class Sum(private val values: Collection<Int>) {
-
     fun value() = values.sum()
 }
--- src/main/kotlin/com/lgaertner/euler/usecase/LargestPrimeFactorUseCase.kt
+++ src/main/kotlin/com/lgaertner/euler/usecase/LargestPrimeFactorUseCase.kt
@@ -1,12 +1,11 @@
 package com.lgaertner.euler.usecase
 
-import com.lgaertner.euler.math.Max
 import com.lgaertner.euler.math.PrimeFactors
 
 class LargestPrimeFactorUseCase {
     fun execute(number: Int): Int {
-        val largestPrimeFactor = Max(PrimeFactors(number))
-        return largestPrimeFactor.value()
+        val largestPrimeFactor = PrimeFactors(number).values().max()
+        return largestPrimeFactor
     }
 }
 
